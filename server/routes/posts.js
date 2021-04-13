8910b72333da1ef4b58602d8d5fe9db7699014a5
thebcitguy <wow1@my.bcit.ca>
2021-04-13 11:17:34 +0800

    set up 'create post function'

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
+import { getPosts, createPost } from '../controllers/posts.js'
+
 const router = express.Router();
 
 //http://localhost:5000/post
 
-router.get ('/', (req, res) => {
-    res.send('this works');
-});
+router.get('/', getPosts);
+router.post('/', createPost);
 
 export default router;
