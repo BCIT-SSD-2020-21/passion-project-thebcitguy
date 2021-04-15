a0c431b79d06e4b705de230a9bc1ae8496d039aa
thebcitguy <wow1@my.bcit.ca>
2021-04-15 18:01:47 +0800

    securely storing connection_url to .env

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js'
 
 const app = express ();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,11 +14,11 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 //mongoDB connection URL and setting port to listen
-const CONNECTION_URL = 'mongodb+srv://admin:s65L9soOQC37VCH8@cluster0.hdsqs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+//const CONNECTION_URL = 'mongodb+srv://admin:s65L9soOQC37VCH8@cluster0.hdsqs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
 //use mongoose to connect to database
-mongoose.connect(CONNECTION_URL,  { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL,  { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
